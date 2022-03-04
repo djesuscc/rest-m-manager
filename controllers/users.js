0984950f53bec7e2198ee661d0b06f3ea89a4d7a
djesuscc <colmenarescdaniel@gmail.com>
2022-03-04 11:46:27 -0600

    Validation in post controller user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const User = require('../database/users');
 
 const get = (req = request, res = response) => {
     const { q, name } = req.query;
@@ -9,12 +12,47 @@ const get = (req = request, res = response) => {
     });
 }
 
-const post = (req, res = response) => {
-    const { name, birthDay } = req.body;
-    res.status(201).json({
-        msg: "post API",
-        ...req.body,
-    });
+const post = async (req, res = response) => {
+    const { name, email, password, role } = req.body;
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ) {
+        return res.status(400).json(errors);
+    }
+    try {
+        const user = new User({
+            name,
+            email,
+            password,
+            role,
+        });
+    
+        // Verify if email exists
+        const existEmail = await User.findOne({ email });
+        if ( existEmail ) {
+            return res.status(400).json({
+                msg: "Email already exists!",
+                email,
+            })
+        }
+    
+        // Encrypt password
+        const salt = bcryptjs.genSaltSync();
+        user.password = bcryptjs.hashSync(password, salt);
+    
+        // Store db
+        await user.save();
+    
+        res.status(201).json({
+            msg: "post API",
+            ...req.body,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "An error ocurred",
+            data: req.body,
+        })
+    }
 }
 
 const put = (req, res = response) => {
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const {
     get,
     post,
@@ -12,7 +13,12 @@ router.get('/', get);
 
 router.put('/:id', put);
 
-router.post('/', post);
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email not valid').isEmail(),
+    check('password', 'Password should have 6 at least 6 characters').isLength({ min: 6 }),
+    check('role', 'Not a valid role').isIn(['ADMIN', 'USER']),
+], post);
 
 router.delete('/', drop);
 
